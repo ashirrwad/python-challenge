9adda949aacfc946fc3d8bd72430c274a664c731
Ashirwad Mishra <ashirwad.golu@gmail.com>
2021-01-13 23:14:03 +0530

    level 1 done

---
+++ Level_1.py
@@ -0,0 +1,5 @@
+result = ""
+string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle grgl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+cypher = str.maketrans("abcdefghijklmnopqrstuvwxyz", "cdefghijklmnopqrstuvwxyzab")
+result += string.translate(cypher)
+print(result)
